d45f54094fa78c42605469be29fe366c42ea00cd
Nasir Mustapha <teboy.com@gmail.com>
2023-10-11 11:14:42 +0100

    add classwork

---
+++ fundamentals/classwork/classwork1.py
@@ -0,0 +1,69 @@
+#1. Give 6 examples of valid variables
+numbers = "123"
+numbers2 = "4"
+name = "Ahmad"
+place = "Isa Kaita"
+job = "carpenter"
+person = "Baby"
+
+#2. Give 6 examples of invalid variables
+# 34people = "Ahmad, "
+# 7players = "Bello"
+# @class = 2
+# -computer = "HP"
+# +name = "Musa"
+# =person = -1
+# class = "Something else"
+#3. Give 6 examples of a string
+name = "Her name"
+place = "Kaduna"
+country = "Japan"
+market = "Kawo Market"
+hospital = "44"
+person = """Aisha"""
+#4. Give 6 examples of an integer
+numbers = 34
+values = 666
+age = 19
+currency_balance = 30
+price = 10
+person = -10
+#5. Give 6 examples of a float
+height = 2.5
+weight = 10.6
+length = 16.5
+size = 26.2
+kg = 20.5
+meters = 11.6
+#6. Give 6 examples of a boolean
+itsRaining = True
+itsHot = True
+heIsMad = False
+nigeriaIsFaulty = False
+itsCloudy = False
+IamHappy = True
+#7. Give 6 examples of a list
+friends = [1, 2, 3, 4]
+my_numbers = [50, 70]
+his_name = ["Ahmad"]
+computers = ["HP", "DELL"]
+names = ["Musa", "Isa"]
+person = [3, "Aisha"]
+#8. Give 6 examples of a tuple
+person = ("Aisha", "Fatima")
+friends = ("Ahmad", "Abubakar")
+countries = ("Nigeria", "Canada")
+states = ("New York", "Chicago")
+villages = "Zaria", "Kachia", "Kaduna"
+people = (1, 2, "Ahmad")
+#9. Give 6 examples of a dictionary
+names = {"city":"lagos"}
+persons = {"name":"alameen", "age":15}
+countries = {"name":"Nigeria", "david":50}
+place = {"Kaduna":"Kawo"}
+age = {"Aisha":"3"}
+people = {"doctor":"Musa", "footballer":"Yakubu"}
+#10. Give 6 examples of variable names that are not recommended
+print = "Ahmad"
+type = "James"
+range = "true"
