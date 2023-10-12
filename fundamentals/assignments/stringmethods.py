860af30957067a887a374ffe121e7cb46b4925bd
Nasir Mustapha <teboy.com@gmail.com>
2023-10-12 11:16:06 +0100

    string methods assingment

---
+++ fundamentals/assignments/stringmethods.py
@@ -0,0 +1,17 @@
+# For each of the following string methods,
+# explain how it works with an example
+# 1. find
+# 2. format
+# 3. index
+# 4. isalnum
+# 5. isalpha
+# 6. isdecimal
+# 7. islower
+# 8. isupper
+# 9. upper
+# 10. lower
+# 11. replace
+# 12. split
+# 13. strip
+# 14. join
+# 15. istitle
