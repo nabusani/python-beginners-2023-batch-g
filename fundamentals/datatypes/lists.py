e55a172ef891ce5b3600ad14716271920186d955
Nasir Mustapha <teboy.com@gmail.com>
2023-10-16 11:21:46 +0100

    list slicing operation

--- fundamentals/datatypes/lists.py
+++ fundamentals/datatypes/lists.py
@@ -9,3 +9,36 @@ print(people)
 print(items)
 
 # METHODS
+
+people.append("Aliyu")
+people.append("Musa")
+people.append("Samuel")
+print(people)
+
+people.clear()
+print(people)
+
+people.append("Samuel")
+people.append("Samuel")
+people.append("Samari")
+print(people)
+print("BEFORE COPY")
+
+people2 = people.copy()
+print(people)
+print(people2)
+
+people.clear()
+
+print(people)
+print(people2)
+
+print(people2.count('Ahmad'))
+print(people2.count('Samuel'))
+
+# LIST OPERATIONS
+print(people2[0])
+print(people2[1])
+print(people2[2])
+print(people2[1:3])
+print(people2[::-1])
