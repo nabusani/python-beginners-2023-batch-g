e5cb4dce83e36f092f14fbd25f27e732cc9d894b
Nasir Mustapha <teboy.com@gmail.com>
2023-10-17 10:26:06 +0100

    list methods and classwork

---
+++ fundamentals/assignments/classwork.py
@@ -0,0 +1,4 @@
+# Q1. Create a variable to hold the string "AHMAD" and change all occurances of
+# "A" with "O"
+# Q2. Create an empty list and append all items in the above variable using the
+# appropraite list method
--- fundamentals/assignments/listmethods.py
+++ fundamentals/assignments/listmethods.py
@@ -2,9 +2,40 @@
 # explain how it works with an example
 
 # 1. extend
+items = [1, 2, 3, 2]
+# items.extend("aliyu".upper())
+# items.extend(["A", "B", "C"])
+items.extend(["Test", 50, 50, 1.5, True, False, [1, 2], (3, 4), {"name":"Omale"}])
+print(items)
 # 2. pop
+x = items.pop(0)
+y = items.pop()
+a = items.pop()
+b = items.pop()
+c = items.pop()
+d = items.pop()
+z = items.pop()
+print(y)
+print(c)
+print(items)
 # 3. remove
+items.remove(50)
+print(items)
 # 4. index
+print(items.index(2))
+print(items.index(2, 1))
 # 5. insert
+items.insert(0, 1)
+items.insert(5, 50)
+print(items)
 # 6. reverse
+items.reverse()
+print(items)
 # 7. sort
+items.pop(2)
+items.sort(reverse=True)
+print(items)
+
+items2 = ["Umar", "Ahmad", "Sani", "Bala"]
+items2.sort(reverse=True)
+print(items2)
