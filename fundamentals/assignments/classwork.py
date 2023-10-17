6c6e5e781c8b98d5e20be04f728b259232211d70
Nasir Mustapha <teboy.com@gmail.com>
2023-10-17 10:52:33 +0100

    few more questions

--- fundamentals/assignments/classwork.py
+++ fundamentals/assignments/classwork.py
@@ -13,3 +13,6 @@ print(chars)
 
 # Q3. Using list slicing, get the first two characters from the list above
 # Q4. Without using the reverse method, reverse the list above
+# Q5. Using the reversed list, create a new string and store it in a new variable
+# Q6. Create an empty list and append individual characters from the new string
+# Q7. Remove the last character from the list and store it in a new variable
