4b0e914f2d89a127d276be97d7728d322aaad4b7
Nasir Mustapha <teboy.com@gmail.com>
2023-10-17 10:46:27 +0100

    correction

--- fundamentals/assignments/classwork.py
+++ fundamentals/assignments/classwork.py
@@ -1,4 +1,12 @@
 # Q1. Create a variable to hold the string "AHMAD" and change all occurances of
 # "A" with "O"
+
+name = "AHMAD"
+name = name.replace("A", "O")
+print(name)
+
 # Q2. Create an empty list and append all items in the above variable using the
 # appropraite list method
+chars = []
+chars.extend(name)
+print(chars)
