fe040cacfa92d872174c5c678d4ddac1b138ea81
Nasir Mustapha <teboy.com@gmail.com>
2023-10-18 10:07:16 +0100

    more classwork questions

--- fundamentals/classwork/classwork2.py
+++ fundamentals/classwork/classwork2.py
@@ -1,4 +1,6 @@
 # Q1. Create a tuple holding 3 items
 # Q2. Using the tuple above, create a new string
 # Q3. Create an empty list and append individual items from the tuple
-# Q4. Append the string to the new list
+# Q4. Append the uppercase version of the string to the new list
+# Q5. Create a new list and append lower case characters from the last item of the new list
+# Q6. Count the number of occurences of the first item in our main tuple
