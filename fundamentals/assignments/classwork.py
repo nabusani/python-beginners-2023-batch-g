252e92af207b56253763ebed5f7530976de6ac2c
Nasir Mustapha <teboy.com@gmail.com>
2023-10-18 09:40:54 +0100

    corrections

--- fundamentals/assignments/classwork.py
+++ fundamentals/assignments/classwork.py
@@ -12,7 +12,17 @@ chars.extend(name)
 print(chars)
 
 # Q3. Using list slicing, get the first two characters from the list above
+print(chars[:2])
 # Q4. Without using the reverse method, reverse the list above
+chars = chars[::-1]
+print(chars)
 # Q5. Using the reversed list, create a new string and store it in a new variable
+new_string = "".join(chars)
+print(new_string)
 # Q6. Create an empty list and append individual characters from the new string
+new_list = []
+new_list.extend(new_string)
+print(new_list)
 # Q7. Remove the last character from the list and store it in a new variable
+new_variable = new_list.pop()
+print(new_variable)
