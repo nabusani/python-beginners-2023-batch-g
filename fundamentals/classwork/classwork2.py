f6fd43c7b47657d9e7a7f7418eec62cb22fc0447
Nasir Mustapha <teboy.com@gmail.com>
2023-10-18 10:03:39 +0100

    tuple classwork

---
+++ fundamentals/classwork/classwork2.py
@@ -0,0 +1,4 @@
+# Q1. Create a tuple holding 3 items
+# Q2. Using the tuple above, create a new string
+# Q3. Create an empty list and append individual items from the tuple
+# Q4. Append the string to the new list
