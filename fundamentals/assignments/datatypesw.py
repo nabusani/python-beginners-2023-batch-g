7fd7e805f0764bd1a48a217c43add5aec9d2a8ff
Nasir Mustapha <teboy.com@gmail.com>
2023-10-18 11:00:22 +0100

    datatypes assigment

---
+++ fundamentals/assignments/datatypesw.py
@@ -0,0 +1,10 @@
+# 1. Create a variable to hold "A2Z"
+# 2. Create a new string to hold the middle item from the above string
+# 3. Create a tuple of the two strings above
+# 4. Create a list to hold the items from the above tuple
+# 5. Add two boolean values to the end of the new list one for true the other for false
+# 6. Create an integer to store the current year
+# 7. Create a float to store your exagerated weight
+# 8. Add the above integer and float to the previous list
+# 9. Create a new list to hold the last 4 items from the old list
+# 10. Reverse the new list with a method and reverse it back without a method
