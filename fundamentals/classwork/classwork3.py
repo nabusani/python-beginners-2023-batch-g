3f9882de43e174d5315eb98da9e31dd97a90fa65
Nasir Mustapha <teboy.com@gmail.com>
2023-10-19 11:08:25 +0100

    dictionary classwork

---
+++ fundamentals/classwork/classwork3.py
@@ -0,0 +1,12 @@
+# Q1. Create an empty dictionary
+# Q2. Use a method to add a new value for the key "name"
+# Q3. Create a new variable and store a tuple of the current key value pair from our dictionary
+#     such that the dictionary now becomes empty.
+# Q3. Use the appropraite method to update the dictionary back to its original form using our
+#     new variable holding its original values
+# Q4. Use the right method to get and print out the "name" from the restored dictionary
+# Q5. Using operation similar to list, add values for each of the following keys into th dictionary:
+#       - "age". "height". "weight", "hobbies"
+# Q6. Create a mew variable to store the value returned from removing hobbies
+# Q7. Use the appropraite method to remove all items from the dictionary
+# Q8. Repeat Q2-Q6 here
