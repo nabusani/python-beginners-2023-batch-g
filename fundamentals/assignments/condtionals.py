3bff601d1de9daf9c4596fd7ce4188509f85e37e
Nasir Mustapha <teboy.com@gmail.com>
2023-10-23 12:35:04 +0100

    conditional assignment

---
+++ fundamentals/assignments/condtionals.py
@@ -0,0 +1,11 @@
+# Q1. Write a website simulator that allows users to signup and signin.
+#     Conditions for signup:
+#           - Name
+#           - Email(unique)
+#           - Phone(unique)
+#           - Password(atleast 5 characters)
+#     Conditions for signin:
+#           - Must have signed up already
+#           - Email must be correct - case insensitive
+#           - Password must be correct
+# NOTE: At each stage notify the user witht the proper message
