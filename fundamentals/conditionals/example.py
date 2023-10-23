e8f1fd6b95caeab7cdca2a981037536ef1933bba
Nasir Mustapha <teboy.com@gmail.com>
2023-10-23 12:30:24 +0100

    introduction to conditonals

---
+++ fundamentals/conditionals/example.py
@@ -0,0 +1,9 @@
+if 'a' in 'Ahmed' and 1 > 0:
+    print("My name is Nur")
+    print("I am 20 years old")
+elif 1 > 2:
+    print("I love potatoes")
+elif 14 > 21 or 1 > 0:
+    print("I love coconuts")
+else:
+    print("Today is a good day")
