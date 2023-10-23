c2a1570edb43096ffaf2956a91e508d8208ca7fb
Nasir Mustapha <teboy.com@gmail.com>
2023-10-23 11:42:28 +0100

    operators classwork correction

--- fundamentals/classwork/classwork4.py
+++ fundamentals/classwork/classwork4.py
@@ -1,5 +1,22 @@
 # Q1. Create 4 variables and add them all together and print the result
+a = 1
+b = 2
+c = 3
+d = 4
+print(a+b+c+d)
 # Q2. Create a variable with the value 6 and update it to 10 with the appropraite operator
+e = 6
+e+=4
+print(e)
 # Q3. Compare all variables in Q1 above
+print(a == b)
+print(a == c)
+print(a == d)
+print(b == c)
+print(b == d)
 # Q4. Create a shopping list and check if you have 'mango' in there
+f = ['cake', 'cookie', 'biro', 'orange']
+print('mango' in f)
 # Q5. Chain all comparison in Q3 above with all our logical operators
+print(a == b and a == c and a == d and b == c and b == d)
+print(a == b or a == c or a == d or b == c or b == d)
